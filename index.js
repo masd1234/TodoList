01bc21c3919fb3da4775de8fff7eb8b905e8a52e
myusername <myemail>
2020-06-22 22:58:51 -0300

    remove function added, css key frames and clear the input buttom when it submmit

--- index.js
+++ index.js
@@ -3,24 +3,23 @@ const inputt = document.querySelector(".inp");
 const buttonMas = document.querySelector(".buttonPlus");
 const buttonMinus= document.querySelector(".buttonMinus");
 
+
+
+const addElement = function(event){
   
-
-
-
-
-const addElement = function(){
+    let getVal = document.querySelector("#val");
     // html elements creation
     const creatCont= document.createElement("div");
-    const creatInput= document.createElement("input");
+    const creatLi= document.createElement("li");
+    creatLi.innerHTML = getVal.value ;
     const creatButtonPlus= document.createElement("button");
     const creatButtonMinus = document.createElement("button");
-
+    const x = document.querySelector("#val").value;
     
-
     //adding classes to html elements
     creatCont.classList.add("todoClone");
-    creatInput.classList.add("inpClone");
-    creatButtonPlus.classList.add("buttonPlusClone");
+    creatLi.classList.add("liClone");
+    const cloneB = creatButtonPlus.classList.add("buttonPlusClone");
     creatButtonMinus.classList.add("buttonMinusClone");
 
 
@@ -32,18 +31,11 @@ const addElement = function(){
     document.body.appendChild(creatCont);
 
     //appending the inner elements to the div
-    creatCont.appendChild(creatInput);
+    creatCont.appendChild(creatLi);
     creatCont.appendChild(creatButtonPlus);
     creatCont.appendChild(creatButtonMinus);
-
-  
-
-   
-    
+    getVal.value = "";    
     console.log('this was add');
-
-   
-    
     
 };
 
@@ -51,14 +43,13 @@ const addElement = function(){
 const removeElement = function(){
    const todoClo = document.querySelector('.todoClone');
    todoClo.parentNode.removeChild(todoClo)
-    
-
-   
-
     console.log('this was deleted');
-    
-    
 };
+
+
+
+
+
 
 
 
@@ -66,6 +57,7 @@ const removeElement = function(){
 
 buttonMas.addEventListener("click", addElement)
 buttonMinus.addEventListener("click", removeElement)
+
 
 
 
