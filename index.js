846c8c65cdea418defecaa4bed2990fcfe546cdb
myusername <myemail>
2020-07-06 23:10:00 -0300

    input field read teh values and save them

--- index.js
+++ index.js
@@ -5,45 +5,90 @@ const buttonMinus= document.querySelector(".buttonMinus");
 
 
 
+
 const addElement = function(event){
+
+    
+    
   
     let getVal = document.querySelector("#val");
     // html elements creation
-    const creatCont= document.createElement("div");
+    const creatCont= document.createElement("div") ;
     const creatLi= document.createElement("li");
-    creatLi.innerHTML = getVal.value ;
+    creatLi.innerHTML = getVal.value;
     const creatButtonPlus= document.createElement("button");
     const creatButtonMinus = document.createElement("button");
+    // read the input field
     const x = document.querySelector("#val").value;
     
     //adding classes to html elements
-    creatCont.classList.add("todoClone");
-    creatLi.classList.add("liClone");
-    const cloneB = creatButtonPlus.classList.add("buttonPlusClone");
-    creatButtonMinus.classList.add("buttonMinusClone");
+    const classCreate = {
+      classCont: creatCont.classList.add("todoClone"),
+      classLi: creatLi.classList.add("liClone"),
+      classPlus:creatButtonPlus.classList.add("buttonPlusClone"),
+      classMinus: creatButtonMinus.classList.add("buttonMinusClone")
+    };
+    
 
 
     //adding inner texts to html elements
     creatButtonPlus.innerText = 'v';
     creatButtonMinus.innerText = 'x';
-    
-    //appending the new div to the body of html
-    document.body.appendChild(creatCont);
 
-    //appending the inner elements to the div
-    creatCont.appendChild(creatLi);
-    creatCont.appendChild(creatButtonPlus);
-    creatCont.appendChild(creatButtonMinus);
-    getVal.value = "";    
+    // adding id
+    creatCont.id = 'todoIdClone';
+
+    
+    
+    
+    
+    if(x === '' || x === null){
+        alert("You must type a task")
+
+    } else {
+        //appending the new div to the body of html
+        document.body.appendChild(creatCont);
+        //appending the inner elements to the div
+        creatCont.appendChild(creatLi);
+        creatCont.appendChild(creatButtonPlus);
+        creatCont.appendChild(creatButtonMinus);
+        getVal.value = "";  
+      
+    }
+
+    
     console.log('this was add');
+
+    
+
+
+
+   
+
+    if(buttonMinus.style.display ='none'){
+        buttonMinus.style.display ='inline-block';
+
+        
+    };
+
+   
+
+    
+   
+    
     
 };
 
 
 const removeElement = function(){
-   const todoClo = document.querySelector('.todoClone');
-   todoClo.parentNode.removeChild(todoClo)
-    console.log('this was deleted');
+   
+   document.body.removeChild(document.body.lastElementChild)
+
+
+   if(document.body.lastElementChild === container ){
+    buttonMinus.style.display ='none';
+   }
+    console.log(addElement.todoClo);
 };
 
 
@@ -54,8 +99,7 @@ const removeElement = function(){
 
 
 
-
-buttonMas.addEventListener("click", addElement)
+buttonMas.addEventListener("click" , addElement )
 buttonMinus.addEventListener("click", removeElement)
 
 
