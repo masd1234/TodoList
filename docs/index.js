19047a55ff7b18b0b85a824a64a6b453a2718b2b
myusername <myemail>
2020-10-14 22:53:59 -0300

    refactored code using objects and iifes to make more robust the app

--- docs/index.js
+++ docs/index.js
@@ -1,70 +1,111 @@
-const container = document.querySelector(".todo1");
-const inputt = document.querySelector(".inp");
-const buttonMas = document.querySelector(".buttonPlus");
-const buttonMinus = document.querySelector(".buttonMinus");
-
-/* this function create a li element if you type a value in the input field  and click the buttom +, if 
-there is no value no list element will be added */
+// iife that creates the selectors for every necessary html element
+const selectorExposer = (() => {
+  let selectors = {
+    container: document.querySelector(".todo1"),
+    inputt: document.querySelector(".inp"),
+    buttonMas: document.querySelector(".buttonPlus"),
+    buttonMinus: document.querySelector(".buttonMinus"),
+  };
+  return {
+    selectorPublic() {
+      return {
+        selectors,
+      };
+    },
+  };
+})();
 
 const addElement = (event) => {
-  const getVal = document.querySelector("#val");
+  const externalSeleAdd = selectorExposer.selectorPublic().selectors;
+
   // html elements creation
-  const creatCont = document.createElement("div");
-  const creatLi = document.createElement("li");
-  const creatButtonPlus = document.createElement("button");
-  const creatButtonMinus = document.createElement("button");
+
+  const containerCreator = {
+    creatCont: document.createElement("div"),
+    creatLi: document.createElement("li"),
+    creatButtonPlus: document.createElement("button"),
+    creatButtonMinus: document.createElement("button"),
+  };
+
+  const {
+    creatCont,
+    creatLi,
+    creatButtonPlus,
+    creatButtonMinus,
+  } = containerCreator;
 
   //adding classes to html elements
 
-  const classCont = creatCont.classList.add("todoClone");
-  const classLi = creatLi.classList.add("liClone");
-  const classPlus = creatButtonPlus.classList.add("buttonPlusClone");
-  const classMinus = creatButtonMinus.classList.add("buttonMinusClone");
+  const classAdder = {
+    classCont: creatCont.classList.add("todoClone"),
+    classLi: creatLi.classList.add("liClone"),
+    classPlus: creatButtonPlus.classList.add("buttonPlusClone"),
+    classMinus: creatButtonMinus.classList.add("buttonMinusClone"),
+  };
 
   // adding id
+
   creatCont.id = "todoIdClone";
   creatButtonPlus.id = "buttonPlusCloneId";
   creatButtonMinus.id = "buttonMinusCloneId";
   creatLi.id = "liCloneId";
-  // read the input field
-  const valueInpt = document.querySelector("#val").value;
-  creatLi.innerHTML = getVal.value;
+
+  // reading the input field
+
+  const getVal = {
+    getValue: document.querySelector("#val"),
+  };
+
+  const valueInpt = getVal.getValue.value;
+  creatLi.innerHTML = getVal.getValue.value;
 
   //adding inner texts to html elements
   creatButtonPlus.innerText = "v";
   creatButtonMinus.innerText = "x";
 
+  //checks if the input is empty or not
   if (valueInpt === "" || valueInpt === null) {
     event.stopPropagation();
     alert("You must type a task in the input field");
   } else {
     //appending the new div to the body of html
-    container.appendChild(creatCont);
+    externalSeleAdd.container.appendChild(creatCont);
     //appending the inner elements to the div
     creatCont.appendChild(creatLi);
     creatCont.appendChild(creatButtonPlus);
     creatCont.appendChild(creatButtonMinus);
-    getVal.value = "";
+    getVal.getValue.value = "";
   }
 };
 
-// this function remove a list element from the bottom click the buttom -
-const removeElement = () => {
-  if (container.lastElementChild === buttonMinus) {
-    event.stopPropagation();
-  } else {
-    container.removeChild(container.lastElementChild);
-  }
-};
+const listenerEvent = ((e) => {
+  const externalSelecListener = selectorExposer.selectorPublic().selectors;
 
-// this function removes the current list element if you click the buttom - or check the same list element if is complete
-container.addEventListener("click", (e) => {
-  if (e.target.id == "buttonMinusCloneId") {
-    e.target.parentNode.remove();
-  } else if (e.target.id == "buttonPlusCloneId") {
-    e.target.parentNode.classList.toggle("taskDone");
-  }
-});
+  // this function remove a list element from the bottom click the buttom
 
-buttonMas.addEventListener("click", addElement);
-buttonMinus.addEventListener("click", removeElement);
+  const removeElement = (e) => {
+    if (
+      externalSelecListener.container.lastElementChild ===
+      externalSelecListener.buttonMinus
+    ) {
+      e.stopPropagation();
+    } else {
+      externalSelecListener.container.removeChild(
+        externalSelecListener.container.lastElementChild
+      );
+    }
+  };
+
+  const checkValue = (e) => {
+    if (e.target.id == "buttonMinusCloneId") {
+      e.target.parentNode.remove();
+    } else if (e.target.id == "buttonPlusCloneId") {
+      e.target.parentNode.classList.toggle("taskDone");
+    }
+  };
+
+  // this function removes the current list element if you click the buttom - or check the same list element if is complete
+  externalSelecListener.container.addEventListener("click", checkValue);
+  externalSelecListener.buttonMas.addEventListener("click", addElement);
+  externalSelecListener.buttonMinus.addEventListener("click", removeElement);
+})();
